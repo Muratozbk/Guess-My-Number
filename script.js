86268a1c15751256450301e28235c471eccc568e
murat <slipstream11@yandex.com>
2023-02-01 23:19:02 +0300

    numbers commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = '🎉 Correct Number!';
@@ -10,5 +10,60 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+// V:74 IMPORTANT:
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
-// 73
+document.querySelector('.check').addEventListener
+    ('click', function () {
+        const guess = Number(document.querySelector('.guess').value);
+        // document.querySelector('.message').textContent =
+        //     '🎉 Correct Number!';
+        console.log(guess, typeof guess);
+        if (!guess) {
+            document.querySelector('.message').textContent =
+                '⛔ No number!';
+        } else if (guess === secretNumber) {
+            document.querySelector('.message').textContent =
+                '🎉 Correct Number!';
+
+        } else if (guess > secretNumber) {
+            if (score > 1) {
+                document.querySelector('.message').textContent =
+                    '👆 Too High!';
+                score--;
+                document.querySelector('.score').textContent = score;
+            } else {
+                document.querySelector('.message').textContent =
+                    '💀 Game Over!';
+                document.querySelector('.score').textContent = 0;
+            }
+
+
+
+        } else if (guess < secretNumber) {
+            if (score > 1) {
+                document.querySelector('.message').textContent =
+                    '👇 Too Low!';
+                score--;
+                document.querySelector('.score').textContent = score;
+            } else {
+                document.querySelector('.message').textContent =
+                    '💀 Game Over!';
+                document.querySelector('.score').textContent = 0;
+            }
+
+        }
+    });
+
+//v75  v74:15 score game over counter
+
+// else if (guess - secretNumber === 1) {
+//     document.querySelector('.message').textContent =
+//         '👆 High!';
+//     score--;
+//     document.querySelector('.score').textContent = score;
+
+// }
