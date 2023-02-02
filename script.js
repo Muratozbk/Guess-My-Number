18d6f0ca85cf712ce9ac252f6d8612616b03b021
murat <slipstream11@yandex.com>
2023-02-02 11:42:23 +0300

    style commit

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 // V:74 IMPORTANT:
+
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 document.querySelector('.number').textContent = secretNumber;
@@ -22,13 +23,21 @@ document.querySelector('.check').addEventListener
         // document.querySelector('.message').textContent =
         //     '🎉 Correct Number!';
         console.log(guess, typeof guess);
+
+        //When there is no input
         if (!guess) {
             document.querySelector('.message').textContent =
                 '⛔ No number!';
+
+            //When Player wins      
         } else if (guess === secretNumber) {
             document.querySelector('.message').textContent =
                 '🎉 Correct Number!';
 
+            document.querySelector('body').style.backgroundColor = '#60b347';
+
+            document.querySelector('.number').style.width = '30rem';
+            //When guess is too high      
         } else if (guess > secretNumber) {
             if (score > 1) {
                 document.querySelector('.message').textContent =
@@ -41,8 +50,7 @@ document.querySelector('.check').addEventListener
                 document.querySelector('.score').textContent = 0;
             }
 
-
-
+            //When guess is too low 
         } else if (guess < secretNumber) {
             if (score > 1) {
                 document.querySelector('.message').textContent =
@@ -58,7 +66,6 @@ document.querySelector('.check').addEventListener
         }
     });
 
-//v75  v74:15 score game over counter
 
 // else if (guess - secretNumber === 1) {
 //     document.querySelector('.message').textContent =
@@ -67,3 +74,8 @@ document.querySelector('.check').addEventListener
 //     document.querySelector('.score').textContent = score;
 
 // }
+
+// const array2 = [Math.trunc(Math.random() * 6) + 1];
+// const array3 = [Math.trunc(Math.random() * 6) + 1];
+// const number4 = array2.concat(array3);
+// console.log(number4, array2[0] + array3[0]);
